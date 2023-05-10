802bf233208f6798c32d96150f2aa4abd6bf6c79
zrtch <1160639597@qq.com>
2023-05-10 22:41:22 +0800

    update

--- url.js
+++ url.js
@@ -1,6 +1,9 @@
 // 获取查询参数，并格式化为js对象
 const url = "www.qq.com?name=zhangsan&age=18&id=000"
+// 使用了 URLSearchParams 对象来解析查询参数，url.split('?')[1]，将 URL 字符串分割成两部分，获取问号后面的查询参数部分
 const params = new URLSearchParams(url.split('?')[1])
+// 使用 entries() 方法获取一个迭代器，遍历每个查询参数的键值对
+// 最后，通过 Object.fromEntries() 方法将键值对数组转换为 JavaScript 对象
 const obj = Object.fromEntries(params.entries())
 console.log(obj);
 
